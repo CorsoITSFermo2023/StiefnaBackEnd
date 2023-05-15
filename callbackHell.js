15f31e8ddeb2688e4c1de27a0daa194b7db29e11
Stefano Andreani <stefanoandreani916@gmail.com>
2023-05-15 16:39:37 +0200

    sium

---
+++ callbackHell.js
@@ -0,0 +1,96 @@
+/**
+ * 
+ * @param {number} input 
+ * @param {function} genoveffa 
+ */
+function primaOperazione(input, genoveffa) {
+    // setTimeout è solo un'esempio
+    setTimeout(() => {
+      if ((input % 5) !== 0) {
+        console.log('errore 5 con ' + input)
+        genoveffa('Errore mio errore 5', undefined);
+      } else {
+        const res = input / 5;
+        console.log('chiamo callback 5 con res ' + res)
+        genoveffa(undefined, res)
+      }
+    }, 0);
+  }
+  
+  function secondaOperazione(input, genoveffa) {
+    // setTimeout è solo un'esempio
+    setTimeout(() => {
+      if ((input % 3) !== 0) {
+        console.log('errore 3 con ' + input)
+        genoveffa('Errore mio errore 3', undefined);
+      } else {
+        const res = input / 3;
+        console.log('chiamo callback 3 con res ' + res)
+        genoveffa(undefined, res)
+      }
+    }, 0);
+  }
+  
+  const genoveffa2 = function (err2, output2) {
+    if (err2) {
+      console.log('errore 2: ' + err2);
+    } else {
+      console.log('finito ' + output2);
+    }
+  }
+  
+  const genoveffa1 = function (err1, output1) {
+    if (err1) {
+      console.log('errore: ' + err1);
+    } else {
+      secondaOperazione(output1, genoveffa2)
+    }
+  
+  }
+  
+const primaPromise = (input) => {
+    const prom = new Promise (
+       (resolve,reject) => {
+        primaOperazione(input, (err1, output) => {
+            if (err1){
+                reject(err1);
+            }
+            else{
+              resolve(output);
+            }
+        })
+       }
+    )
+    return prom; 
+ }
+
+ const secondaPromise = (input) => {
+  const prom = new Promise(
+    (resolve,reject) => {
+      secondaOperazione(input, (err2, output) =>{
+        if(err2) {
+          reject(err2);
+        }
+        else{
+          resolve(output)
+        }
+      } )
+    }
+  )
+  return prom;
+ }
+
+primaPromise(20)
+.then(
+  (risultato) => {
+    return secondaPromise(risultato)
+  }
+).then(
+  (risultato2) => {
+    console.log("Finito con risultato "+risultato2)
+  }
+).catch(
+  (errore) => {
+    console.log("Mega Errore: ",errore);
+  }
+);
